d0d940be5589f512dd72839ffbae0a66ce4358df
praveentechie <praveen.arunachalam74@gmail.com>
2021-04-27 00:51:15 +0530

    added prod express server

---
+++ angular8/angular-base/server.js
@@ -0,0 +1,20 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/angular-base'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/angular-base/'});
+});
+
+app.listen(process.env.PORT || 8080);
+console.log('Running app in ', process.env.PORT || 8080);
